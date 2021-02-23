0f0c6ce13e7d4146867326f301a7900bae00233e
codeslw <sanvarkhujaev@gmail.com>
2021-02-23 17:35:33 +0500

    second attempts

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,17 @@
 import { StylesProvider, CssBaseline } from "@material-ui/core";
 import React from "react";
 import { Sample } from "./components/Sample";
-
+import Navbar from './components/Navbar/Navbar'
+import {BrowserRouter,Route,Switch} from 'react-router-dom'
 const  App: React.FC = () =>{
   return (
+    <BrowserRouter>
     <div>
-      
+      <Navbar/>
+    
     </div>
+    </BrowserRouter>
+    
   );
 }
 
